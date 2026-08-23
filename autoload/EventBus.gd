f1d51715314cca6f63cb62020c9a13b8b8c309cb
gothat-netizen <got.hat@gmail.com>
2026-08-22 20:12:14 -0700

    Delete autoload directory

--- autoload/EventBus.gd
+++
@@ -1,4 +0,0 @@
-## Autoload wrapper around core/events/event_bus.gd's EventBusImpl.
-## Registered as a singleton in project.godot -- access globally as
-## `EventBus.emit_event(...)` / `EventBus.game_event` signal.
-extends "res://core/events/event_bus.gd"
