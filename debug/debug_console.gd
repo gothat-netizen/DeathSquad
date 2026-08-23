488422eafb3e1559f5f1c5f55a773f7e72c79ea7
gothat-netizen <got.hat@gmail.com>
2026-08-22 20:12:57 -0700

    Delete debug directory

--- debug/debug_console.gd
+++
@@ -1,7 +0,0 @@
-## Debug console: force dice results, spawn/move operatives, advance
-## turning points, trigger abilities/missions, reset the battlefield.
-## Gated behind DebugOverlay.enabled.
-##
-## PHASE: 1 (this stub); commands added alongside the systems they debug.
-class_name DebugConsole
-extends Node
